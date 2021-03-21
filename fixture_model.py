f711abc84a3bc49d9a15638518fde8b2b4847f3b
Amilkar Munoz <yellowballpython@gmail.com>
2021-03-21 04:11:56 -0600

    Methods added to Fixture

---
+++ fixture_model.py
@@ -0,0 +1,38 @@
+import json
+
+class Fixture():
+    def __init__(self, app, model, headers=None, list_of_fields=None):
+        self.app = app
+        self.model = model
+        self.headers = headers
+        self.fields = list_of_fields
+        self.objects = []
+        self.object_ = {}
+        self.body = {}
+        self.json_filename = 'carraspera'
+
+    def create_objects(self):
+        pk = 0
+        for row in self.fields:
+            pk += 1
+            self.object_ = {}
+            self.body = {}
+            self.object_['model'] = '{}.{}'.format(self.app, self.model).lower()
+            self.object_['pk'] = pk
+            for index, value in enumerate(row):
+                self.body[self.headers[index]] = value
+            self.object_['fields'] = self.body
+            self.objects.append(self.object_)
+
+    def return_objects(self):
+        return self.objects
+        
+    def return_json(self):
+        return json.dumps(self.objects, indent=2)
+    
+    def create_json_file(self):
+        with open(f'{self.json_filename}.json', 'w') as f:
+            f.write(self.return_json())
+
+
+
