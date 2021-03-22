7b5170e757d06b4805b1eb63804f553f2b6b721a
Amilkar Munoz <yellowballpython@gmail.com>
2021-03-22 14:13:51 -0600

    Main function implemented

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import os
 import json
 import openpyxl
-from fixture import Fixture
-from utils import (
-    check_existence,
-    check_extension,
-    total_col_xlsx,
-    get_headers_xlsx,
-)
+from models import Fixture, Menu, Book, Path
 
-# inputs
-filename = input("Nombre del archivo: ")
-extension = input("Extensión del archivo (csv or xlsx) : ")
-# paths
-file_path = os.getcwd() + f'\\{filename}.{extension}'
-results_path = os.getcwd() + f'\\{"results"}'
+def main():
+    path = Path()
+    menu = Menu()
+    menu.instructions()
+    while True:
+        # menu.de_entrada()
+        try:
+            excel_filename = input("Ingresa el nombre del archivo de excel: ")
+            excel_exists = path.check_existence(excel_filename)
+            if excel_exists:
 
+                # File found notification for the user.
+                menu.file_found()
 
+                # Tras haber verificado que el archivo existe, se crea el path para cargar el libro con openpyxl.
+                excel_file_path = path.book_path(excel_filename)
+                
+                # Book settings
+                book = Book(excel_file_path)
+                book_headers = book.get_headers()
+                book_rows = book.get_list_of_rows()
+
+                app_name = input("Ingresa el nombre del app de tu proyecto Django: ")
+                model_name = input("Ingresa el nombre del modelo de tu app: ")
+
+                fx = Fixture(
+                    app=app_name,
+                    model=model_name,
+                    headers=book_headers,
+                    list_of_fields=book_rows,
+                )
+                fx.create_objects()
+                fx.create_json_file(path.json_dir)
+
+                menu.task_completed()
+                break
+            else:
+                menu.file_not_found()
+
+        except ValueError:
+            print("Ingresa el nombre del archivo.")
+            print("")
+            pass
+
+if __name__ == '__main__':
+    main()
